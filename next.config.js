08dd81ab9c70fbe5819af68627104e65df6c2e9e
Heihokon <neoshark22@gmail.com>
2023-06-13 11:42:49 -0500

    add comments

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
-const startFlagshipSDK = require('./startFlagshipSDK')
+// Importing the startFlagshipSDK function from the startFlagshipSDK module
+const startFlagshipSDK = require('./startFlagshipSDK');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-}
+};
 
+// Exporting a function that returns the Next.js configuration object
 module.exports = () => {
-
-  if (process.argv.includes('dev')) {
-    startFlagshipSDK()
+  // Check if the 'dev' or 'start' argument is present in the process arguments
+  if (process.argv.includes('dev') || process.argv.includes('start')) {
+    startFlagshipSDK(); // If it is, start the Flagship SDK
   }
-  return nextConfig
-}
+  return nextConfig; // Return the Next.js configuration object
+};
