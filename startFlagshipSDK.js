55d46a8b2a15270881b61f8c99c424af9eab2941
Heihokon <neoshark22@gmail.com>
2025-01-20 09:36:15 -0600

    refactor: rename startFlagshipSDK to startFlagshipSDKAsync and update decision mode handling

--- startFlagshipSDK.js
+++ startFlagshipSDK.js
@@ -1,52 +1,19 @@
 // Importing required modules from the Flagship React SDK
-const { DecisionMode, Flagship, FlagshipStatus } = require("@flagship.io/react-sdk");
+const { DecisionMode, Flagship, FSSdkStatus } = require("@flagship.io/react-sdk");
 
 // Function to start the Flagship SDK
-function startFlagshipSDK() {
+async function startFlagshipSDKAsync() {
     // Check if the Flagship SDK has already been initialized
-    if (Flagship.getStatus() && Flagship.getStatus() !== FlagshipStatus.NOT_INITIALIZED) {
-        return; // If it has been initialized, return early
+    if (Flagship.getStatus() && Flagship.getStatus() !== FSSdkStatus.SDK_NOT_INITIALIZED) {
+        return Flagship; // If it has been initialized, return early
     }
     // Start the Flagship SDK with the provided environment ID and API key
-    Flagship.start(process.env.NEXT_PUBLIC_ENV_ID, process.env.NEXT_PUBLIC_API_KEY, {
+    return await Flagship.start(process.env.NEXT_PUBLIC_ENV_ID, process.env.NEXT_PUBLIC_API_KEY, {
         fetchNow: false, // Do not fetch flags immediately
-        decisionMode: DecisionMode.DECISION_API // set decision mode : DECISION_API or BUCKETING
+        decisionMode: DecisionMode.BUCKETING // set decision mode : DECISION_API or BUCKETING
     });
 }
 
-// Function to start the Flagship SDK
-function startFlagshipSDKAsync() {
-    // Return a new Promise
-    return new Promise((resolve) => {
-        // Check if the Flagship SDK has already been initialized
-        if (
-            Flagship.getStatus() &&
-            Flagship.getStatus() !== FlagshipStatus.NOT_INITIALIZED
-        ) {
-            // If it has been initialized, resolve the Promise with the Flagship object and return early
-            resolve(Flagship);
-            return;
-        }
-        // If the SDK has not been initialized, start it with the specified parameters
-        Flagship.start(
-            process.env.NEXT_PUBLIC_ENV_ID, // Environment ID
-            process.env.NEXT_PUBLIC_API_KEY, // API key
-            {
-                pollingInterval: 20, // Set polling interval to 20
-                fetchNow: false, // Do not fetch flags immediately
-                decisionMode: DecisionMode.BUCKETING, // Set decision mode to BUCKETING
-                onBucketingSuccess: () => {
-                    // On successful bucketing, resolve the Promise with the Flagship object
-                    resolve(Flagship);
-                },
-                onBucketingFail() {
-                    // On failed bucketing, resolve the Promise with the Flagship object
-                    resolve(Flagship);
-                },
-            }
-        );
-    });
-}
 
 // Export the startFlagshipSDK function
-module.exports = { startFlagshipSDK, startFlagshipSDKAsync };
+module.exports = { startFlagshipSDKAsync };
